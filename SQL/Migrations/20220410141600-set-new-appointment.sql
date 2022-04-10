c5c37b8155e87e7ce3ea19ac913cd5592a5b11a7
tishamIslam <tisham.islam@gmail.com>
2022-04-10 16:06:00 -0400

    Finished up set-new-appointment query

--- SQL/Migrations/20220410141600-set-new-appointment.sql
+++ SQL/Migrations/20220410141600-set-new-appointment.sql
@@ -1,16 +1,17 @@
 -- Set a new appointment for patient id 2 
--- (still needs to be worked on, needs Treatment and Appointment_procedure)
 
 -- not sure if we just need to add a new value in the table 
 -- OR update the values of the existing row of the patient 
 
 -- FIRST OPTION
-INSERT INTO Appointment VALUES
-(3,2,2,TO_DATE('2022-04-28', 'YYYYMMDD'),'14:00:00','15:00:00',1,'Booked',8)
-
--- SECOND OPTION
-UPDATE Appointment
-SET date_of_appointment = '2022-04-28',
-    start_time = '14:00:00', end_time = '15:00:00',
-    appointment_type = 1, appointment_status = 'Booked', room = 8
-WHERE patient_id = 2;
+WITH App AS (
+    INSERT INTO Appointment 
+    VALUES (DEFAULT, 2, 2, TO_DATE('2022 04 28', 'YYYY MM DD'), '14:00:00', '15:00:00', 1, 'Booked', 8)
+    RETURNING appointment_id
+    ), 
+    Treat AS (
+        INSERT INTO Treatment
+        VALUES (DEFAULT, 'Tooth cleaning', 'No medications administered', 'No symptoms', 999, 'The teeth are very dirty', 2, (SELECT appointment_id FROM App))
+    )
+INSERT INTO Appointment_procedure (appointment_id, patient_id, date_of_procedure, procedure_code, appointment_description, tooth, amount_of_procedure, total_charge) 
+    VALUES ((SELECT appointment_id FROM App), 2, TO_DATE('2022 04 28', 'YYYY MM DD'), 1, 'Regular cleaning', 999, 1, 1000);
