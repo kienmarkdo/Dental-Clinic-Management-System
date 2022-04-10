82441dc289b3f5c18e06c05b6742dc0fd7154ec1
tishamIslam <tisham.islam@gmail.com>
2022-04-10 15:33:57 -0400

    Added addemployee and viewprocedures queries

---
+++ SQL/Migrations/addemployee.sql
@@ -0,0 +1,4 @@
+INSERT INTO Employee_info (employee_sin, employee_type, name, address, annual_salary) 
+    VALUES (123123123, 'd', 'Johnny Smith', '123 Middle of Nowhere Avenue', 100);
+INSERT INTO Employee (employee_sin, branch_id) 
+    VALUES (123123123, 1);
