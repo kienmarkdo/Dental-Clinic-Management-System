d789a5e23f70c2fceb27da07dd67189e3f722f85
Celine Wan <mwanm074@uottawa.ca>
2022-04-17 22:38:28 -0400

    Fix case when dentist has no upcoming appointment
    + Added Logout buttons to pages
    PS: Mark will add the logout button to patient page

--- PHP/receptionist_landing.php
+++ PHP/receptionist_landing.php
@@ -20,13 +20,16 @@ $eUsername = $_SESSION['empUName'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DCMS - Patient Homepage</title>
-    <link rel="stylesheet" href="CSS/main.css">
+    <title>DCMS - Receptionist Homepage</title>
+    <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 </head>
 <body>
-    <h1>DCMS - Receptionist <?php echo $eUsername ?> homepage</h1>
-
+    <div class="container">
+        <div class="logout-btn">
+            <a href="logout.php" class="logout-btn-text">Logout</a>
+        </div>
+        <h1>DCMS - Receptionist <?php echo $eUsername ?> homepage</h1>
     </div>
 </body>
 </html>
