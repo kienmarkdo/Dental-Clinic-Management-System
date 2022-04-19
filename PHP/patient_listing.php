323fa74b8888b12e0b8d3fc771992702641a0a87
Kanjanokphat Kitisuwanakul <amyk.kiti@gmail.com>
2022-04-18 21:59:22 -0400

    Added 2 .php files for receptionist

---
+++ PHP/patient_listing.php
@@ -0,0 +1,14 @@
+<?php
+
+ob_start();
+session_start();
+
+include 'functions.php';
+include_once 'db.php';
+error_reporting(0);
+
+echo "PATIENT PROFILE WITH ADD EDIT DELETE OPTION HERE"; //similar to patient_landing, 
+//but with add edit and delete options
+
+
+?>
