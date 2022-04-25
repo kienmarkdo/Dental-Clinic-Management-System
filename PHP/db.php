ff8ecc990f28d8febc10dd42a0dc5134d1e5ea5d
tishamIslam <tisham.islam@gmail.com>
2022-04-25 16:04:41 -0400

    changed db url

--- PHP/db.php
+++ PHP/db.php
@@ -1,5 +1,5 @@
 <?php 
 // Configure the postgres parameters here to your own settings when running locally
 // Do not overwrite this when pushing to git
-$dbconn=pg_connect(getenv("DATABASE_URL")); 
+$dbconn=pg_connect(getenv("HEROKU_POSTGRESQL_ONYX_URL")); 
 ?>
